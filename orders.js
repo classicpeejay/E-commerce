c01e54d2568ed5a5caff8f459e2a5ddde2b992e2
classicpeejay <adepojujonathan66@gmail.com>
2022-08-22 16:48:59 +0100

    E-commerce

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Tradeable Cryptocurrency',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Tradeable Cryptocurrency',
+        productNumber: '85631',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Tradeable Cryptocurrency',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Tradeable Cryptocurrency',
+        productNumber: '85631',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Tradeable Cryptocurrency',
+        productNumber: '85631',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+]
